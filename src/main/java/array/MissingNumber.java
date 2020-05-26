b66ad257978ed739b411a7002aa65c9e64bf14da
anjalipancholi <anjalipancholi8@gmail.com>
2020-05-26 22:43:51 +0530

    Check the code

--- src/main/java/array/MissingNumber.java
+++ src/main/java/array/MissingNumber.java
@@ -1,4 +1,20 @@
 package array;
 
+import java.util.Arrays;
+
 public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums[i]) return i;
+            System.out.println(i);
+        }
+
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        new MissingNumber().missingNumber(new int[]{3, 0, 1});
+    }
 }
