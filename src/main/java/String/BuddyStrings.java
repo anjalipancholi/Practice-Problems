0c90808ba91a39a29f19be70f6b96335706aba28
anjalipancholi <anjalipancholi8@gmail.com>
2020-10-15 09:42:31 +0530

    Problems

---
+++ src/main/java/String/BuddyStrings.java
@@ -0,0 +1,11 @@
+package String;
+
+public class BuddyStrings {
+    public boolean buddyStrings(String A, String B) {
+return true;
+    }
+
+    public static void main(String[] args) {
+        new BuddyStrings().buddyStrings("aaaaaaabc", "aaaaaaacb");
+    }
+}
